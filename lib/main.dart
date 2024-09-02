3f47fe0444a17ace9205d015e6c313191c2020ba
Jing Yu <yu.j@salesforce.com>
2024-09-01 23:24:51 -0700

    get and display user location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,8 +26,88 @@ class GeolocatorApp extends StatefulWidget {
 }
 
 class _GeolocatorAppState extends State<GeolocatorApp> {
+  Position? _currentLocation;
+  late bool servicePermission = false;
+  late LocationPermission permission;
+
+  String _currentAddress = "";
+
+  Future<Position> _getCurrentLocation() async {
+    // check if the user have permission to access location service
+    servicePermission = await Geolocator.isLocationServiceEnabled();
+    if (!servicePermission) {
+      print("service disabled");
+    }
+
+    // when the location service is enabled on the device, check if it has permission
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  // convert the coorninate into address
+  _getAddressFromCoordinates() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(_currentLocation!.latitude, _currentLocation!.longitude);
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress = "${place.locality}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Get Current Location"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Location coordinates", 
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            SizedBox(height: 6),
+            Text("Latitude: ${_currentLocation?.latitude}; Longitude: ${_currentLocation?.longitude}"),
+
+            SizedBox(height: 30),
+
+            Text(
+              "Location address", 
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            SizedBox(height: 6),
+            Text("${_currentAddress}"),
+
+            SizedBox(height: 50),
+            
+            ElevatedButton(
+              onPressed: () async {
+                _currentLocation = await _getCurrentLocation();
+                await _getAddressFromCoordinates();
+                print("${_currentLocation}");
+                print("${_currentAddress}");
+              }, 
+              child: Text("get location")
+            )
+          ],
+      )),
+    );
   }
 }
